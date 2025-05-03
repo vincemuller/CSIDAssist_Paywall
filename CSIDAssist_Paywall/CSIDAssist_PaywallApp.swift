95353f1f1f7f3f70b2c29d635ad7b5b606ce356d
vincemuller <vmuller2529@gmail.com>
2025-05-02 20:41:21 -0700

    Initial Commit

---
+++ CSIDAssist_Paywall/CSIDAssist_PaywallApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSIDAssist_PaywallApp.swift
+//  CSIDAssist_Paywall
+//
+//  Created by Vince Muller on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct CSIDAssist_PaywallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
